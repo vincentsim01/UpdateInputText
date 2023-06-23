00d84c18748c355a9b5611eeb67d3c6976f1e12f
vincentsim01 <129670790+vincentsim01@users.noreply.github.com>
2023-06-23 21:47:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function checkText(){
+    var originalvalue = document.getElementById("getValues").value;
+    console.log("The value in the text box is ="+originalvalue);
+
+};
+
+setInterval(checkText,5000);
+
